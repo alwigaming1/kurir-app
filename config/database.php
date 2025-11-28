b7f40bfe1cc4ceb6d9d30cb48e1cfb87d08421d6
alwigaming1 <142589874+alwigaming1@users.noreply.github.com>
2025-11-28 09:52:45 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -83,15 +83,15 @@ return [
             ]) : [],
         ],
 
-        'pgsql' => [
+       'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('DATABASE_URL'), // <--- Ini harus ada
+            'host' => env('DB_HOST', '127.0.0.1'), // <--- Ini juga harus ada
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
-            'charset' => env('DB_CHARSET', 'utf8'),
+            'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
